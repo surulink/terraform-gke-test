595758dd1cd4e5812ed80f059ae33030e8342f2e
surulink <38012582+surulink@users.noreply.github.com>
2021-11-27 13:00:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,14 +105,4 @@ module "gke" {
     ]
   }
 }
-resource "google_compute_firewall" "ssh-rule" {
-  depends_on   = [module.gke]
-  name = "ssh"
-  network  = "${var.network}-${var.env_name}"
-  project  = "${var.project_id}"
-  allow {
-    protocol = "tcp"
-    ports = ["22"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-}
+
